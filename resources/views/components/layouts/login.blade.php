80c3416661fe07441154a3709e112dc3c21aefa6
Yudo <53389941+juniz@users.noreply.github.com>
2024-07-04 11:49:29 +0700

    comit

--- resources/views/components/layouts/login.blade.php
+++ resources/views/components/layouts/login.blade.php
@@ -30,12 +30,12 @@
 
     {{-- @livewireScripts --}}
     {{-- PWA Script --}}
-    <script src="{{ asset('sdm/sw.js') }}"></script>
+    <script src="{{ asset('sw.js') }}"></script>
     <script>
         if ("serviceWorker" in navigator) {
             // Register a service worker hosted at the root of the
             // site using the default scope.
-            navigator.serviceWorker.register("/sw.js").then(
+            navigator.serviceWorker.register("/sdm/sw.js").then(
             (registration) => {
                 console.log("Service worker registration succeeded:", registration);
             },
