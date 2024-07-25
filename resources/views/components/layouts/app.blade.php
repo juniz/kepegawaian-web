c06b531f2bb05c262c705d55aeaa13c06dde7972
Yudo <53389941+juniz@users.noreply.github.com>
2024-07-25 08:10:35 +0700

    fix hak akses

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -55,7 +55,7 @@
                 
                 <x-menu-item title="Dashboard" icon="o-chart-bar-square" link="/dashboard" />
                 @foreach(config('presensi.dashboard_pegawai') as $item)
-                    @if($user->cap == $item['id'])
+                    @if($user->id == $item['id'])
                         <x-menu-item title="Dashboard Pegawai" icon="o-chart-bar-square" link="/dashboard/pegawai" />
                     @endif
                 @endforeach
