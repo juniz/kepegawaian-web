106b41858ae8ea7b4c64c43b0f3a454680585e85
Yudo <53389941+juniz@users.noreply.github.com>
2024-07-29 14:03:10 +0700

    comit

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,24 @@ use App\Http\Controllers\RapatController;
 use Livewire\Livewire;
 use Livewire\Volt\Volt;
 
-Livewire::setScriptRoute(function ($handle) {
-    return Route::get('vendor/livewire/livewire.js', $handle);
-});
+// Livewire::setScriptRoute(function ($handle) {
+//     return Route::get('vendor/livewire/livewire.js', $handle);
+// });
 
-Livewire::setUpdateRoute(function ($handle) {
-    // dd($handle);
-    return Route::post('vendor/livewire/update', $handle);
-});
+// Livewire::setUpdateRoute(function ($handle) {
+//     // dd($handle);
+//     return Route::post('vendor/livewire/update', $handle);
+// });
 
 Volt::route('/', 'login.index')->middleware('login');
 Volt::route('/home', 'home.index')->middleware('ceklogin');
 Volt::route('/izin', 'izin.index')->middleware('ceklogin');
-// Volt::route('/cuti', 'cuti.index')->middleware('ceklogin');
+Volt::route('/cuti', 'cuti.index')->middleware('ceklogin');
 Volt::route('/jadwal', 'jadwal.index')->middleware('ceklogin');
 Volt::route('/dashboard', 'dashboard.index')->middleware('ceklogin');
 Volt::route('/dashboard/pegawai', 'dashboard.pegawai')->middleware('ceklogin');
-// Volt::route('/rapat', 'rapat.index');
-// Route::get('/rapat/print', [RapatController::class, 'index']);
+Volt::route('/rapat', 'rapat.index');
+Route::get('/rapat/print', [RapatController::class, 'index']);
 Route::get('/logout', function () {
     session()->forget('user');
     return redirect('/');
