d727b2a5b8bac43b8b97a6ee4495a0db8a046a30
Yudo <53389941+juniz@users.noreply.github.com>
2024-08-08 16:22:54 +0700

    comit

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -327,8 +327,9 @@ new class extends Component {
 
                 $tmp->delete();
                 DB::commit();
-                $this->dispatch('refresh');
                 $this->success('Presensi berhasil');
+                $this->cekPresensi();
+                $this->dispatch('refresh');
             }
         }catch(\Throwable $e){
             DB::rollBack();
